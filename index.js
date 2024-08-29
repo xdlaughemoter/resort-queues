a4869b51265c7414b4dea8ce0d6f1d2fdb52f7d9
xdlaughemoter <122099752+xdlaughemoter@users.noreply.github.com>
2024-08-29 20:53:41 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import utils from "../_utils";
+
+
+function resort() {
+    // Step 1: Select the container
+    const container = document.querySelector('.parties-game-type-card-categories');
+
+    // Step 2: Select all div elements with the class "parties-game-type-card-category-div"
+    const divs = container.querySelectorAll('.parties-game-type-card-category-div');
+    
+    // Step 3: Move the last div to the top
+    if(divs[0].children[3].getAttribute('data-queue-id')==400){
+        const lastDiv = divs[divs.length - 1]; // Get the last div
+        container.removeChild(lastDiv); // Remove it from its current position
+        container.insertBefore(lastDiv, container.firstChild); // Insert it at the top
+    }
+    
+    
+    
+}
+let addDodgeAndExitButtonObserver = (mutations) => {
+	if (document.querySelector(".parties-game-type-card-category-div")){
+		resort()
+	}
+}
+
+
+window.addEventListener('load', () => {
+	utils.routineAddCallback(addDodgeAndExitButtonObserver, ["bottom-right-buttons"])
+	utils.addCss("//plugins/dodge_button/assets/dodge_button.css")
+})
+
+
